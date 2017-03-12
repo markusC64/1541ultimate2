8f9f71a62952c5ef0991ebacce211c7014531e66
markusC64 <markusC64@localhost>
2017-03-12 19:52:42 +0100

    Volume COntrol for Internal Speaker

--- fpga/altera/u2p_io.vhd
+++ fpga/altera/u2p_io.vhd
@@ -28,6 +28,7 @@ port (
     eth_irq_i   : in  std_logic;
     hub_reset_n : out std_logic;
     speaker_en  : out std_logic;
+    speaker_vol : out std_logic_vector(3 downto 0);
     ulpi_reset  : out std_logic );
     
 end entity;
@@ -38,6 +39,7 @@ architecture rtl of u2p_io is
     signal i2c_scl_out : std_logic;
     signal i2c_sda_out : std_logic;
     signal speaker_en_i : std_logic;
+    signal speaker_vol_i : std_logic_vector(3 downto 0);
     signal hub_reset_i  : std_logic;
     signal ulpi_reset_i : std_logic;
 begin
@@ -102,6 +104,7 @@ begin
                     mdio_o <= io_req.data(0);
                 when X"C" =>
                     speaker_en_i <= io_req.data(0);
+		    speaker_vol_i <= io_req.data(4 downto 1);
                 when X"D" =>
                     hub_reset_i <= io_req.data(0);
                 when X"E" =>
@@ -127,6 +130,7 @@ begin
 
     mdc     <= mdc_out;
     speaker_en <= speaker_en_i;
+    speaker_vol <= speaker_vol_i;
     hub_reset_n <= not (hub_reset_i or reset);
     ulpi_reset  <= ulpi_reset_i or reset;
     
